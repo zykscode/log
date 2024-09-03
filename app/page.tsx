1d0100d31adaf090066df1364e1bab1ab034c116
zykcode <zyskson@gmail.com>
2024-09-03 22:45:04 +0100

    feat: first

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,11 @@ const Page = async () => {
   return (
     <Wrapper>
       <div className="flex flex-col lg:flex-row">
-        <div className="lg:w-1/3 lg:pr-8 mb-8 lg:mb-0">
-          <h1 className="text-3xl font-bold mb-4">Posts</h1>
-          <p className="text-lg">
+        <div className="lg:w-1/3  lg:pr-8 mb-8 flex flex-col lg:mb-0">
+          <h1 className="relative pt-4 md:pt-8 block z-[999] uppercase text-9xl mb-4">
+            Posts
+          </h1>
+          <p className="text-lg max-w-sm px-4 hidden md:block">
             Welcome to our blog! Here you'll find the latest articles on various
             topics. Scroll through the list on the right to explore our content.
           </p>
